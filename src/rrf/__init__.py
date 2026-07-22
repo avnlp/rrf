038bf32121bcdcf7d4461edd5cc8b4e89dc5b4e7
Ashwin Mathur <97467100+awinml@users.noreply.github.com>
2026-07-22 10:24:22 +0530

    build: Migrate build tooling from Hatch to uv (#7)
    * build: Migrate build tooling from Hatch to uv
    
    * ci: Simplify workflows
    
    * ci: Pin uv setup to v7
    
    * ci: Update workflows
    
    * build: Bump minimum python version

--- src/rrf/__init__.py
+++ src/rrf/__init__.py
@@ -1,4 +1,4 @@
 from rrf.beir_dataloader import BeirDataloader
 from rrf.beir_evaluator import BeirEvaluator
 
-__all__ = ["BeirEvaluator", "BeirDataloader"]
+__all__ = ["BeirDataloader", "BeirEvaluator"]
